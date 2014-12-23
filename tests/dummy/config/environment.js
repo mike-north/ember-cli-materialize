ee072c4c98b3e40204117a4df51c547550ef2a15
Stefan Gasser <stefangasser.info@gmail.com>
2014-12-23 21:16:32 +0100

    Prepare config for gh-pages

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -40,7 +40,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.baseURL = '/ember-cli-materialize';
+    ENV.locationType = 'hash';
   }
 
   ENV.contentSecurityPolicy = {
