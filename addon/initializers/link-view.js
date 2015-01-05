5e947cb146590e2d1a39e8ff28a3f165f07e027c
Miguel <jmandrade0@gmail.com>
2015-01-05 16:37:34 +0000

    reopen LinkView in initializer

---
+++ addon/initializers/link-view.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export function initialize(/* container, application */) {
+  Ember.LinkView.reopen({
+    attributeBindings: ['data-activates'],
+  });
+}
+
+export default {
+  name: 'link-view',
+  initialize: initialize
+};
