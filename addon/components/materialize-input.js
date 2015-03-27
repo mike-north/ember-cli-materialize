b5436330461cd7eaeb3dc185c923bce15e10f4f8
Ryan Connolly <rynam0@gmail.com>
2015-03-27 19:29:17 -0400

    merging from remote tracking branch
    missed 2 files in my previous commit.

---
+++ addon/components/materialize-input.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+import MaterializeInputField from './materialize-input-field';
+import layout from '../templates/components/materialize-input';
+
+export default MaterializeInputField.extend({
+  layout: layout,
+  didInsertElement: function() {
+    this._super();
+    // make sure the label moves when a value is bound.
+    var labelSelector = this.$('>label');
+    if (Ember.isPresent(this.get('value')) && !labelSelector.hasClass('active')) {
+      labelSelector.addClass('active');
+    }
+  }
+});
+
