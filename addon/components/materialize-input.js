f3780b6a99317debdfedf2a14c6128c6d93f16a7
Jaime <venturini.jaime@yahoo.com>
2015-04-02 13:27:58 +0200

    Feature: Set the type of the input

--- addon/components/materialize-input.js
+++ addon/components/materialize-input.js
@@ -4,6 +4,7 @@ import layout from '../templates/components/materialize-input';
 
 export default MaterializeInputField.extend({
   layout: layout,
+  type: '',
   didInsertElement: function() {
     this._super();
     // make sure the label moves when a value is bound.
@@ -11,6 +12,10 @@ export default MaterializeInputField.extend({
     if (Ember.isPresent(this.get('value')) && !labelSelector.hasClass('active')) {
       labelSelector.addClass('active');
     }
+
+    //If no type is set as parameter use "text" as default
+    if(Ember.isEmpty(this.get('type'))) {
+      this.set('type', 'text');
+    }
   }
 });
-
