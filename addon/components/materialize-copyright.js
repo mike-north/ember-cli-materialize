2abf0d53e7b24cd91ab93031a464be4f4851f28c
franktcurran <franktcurran@gmail.com>
2015-04-07 14:34:56 -0400

    Updating copyright date functionality.

--- addon/components/materialize-copyright.js
+++ addon/components/materialize-copyright.js
@@ -2,9 +2,22 @@ import Ember from 'ember';
 import layout from '../templates/components/materialize-copyright';
 
 export default Ember.Component.extend({
+  init: function() {
+    this._super();
+    if (this.get('startYear') != null) {
+      Ember.assert('Property startYear must be less than or equal to the current year.',
+        this.get('startYear') <= new Date().getFullYear());
+    }
+  },
   layout: layout,
   classNames: ['footer-copyright'],
   date: function () {
-    return new Date().getFullYear();
+    var returnDate;
+    if (this.get('startYear') == null || this.get('startYear') == new Date().getFullYear()) {
+      returnDate = new Date().getFullYear();
+    } else {
+      returnDate = this.get('startYear') + ' - ' + new Date().getFullYear();
+    }
+    return returnDate;
   }.property()
 });
