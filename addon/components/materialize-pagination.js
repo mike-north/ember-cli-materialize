3b43cfb9339ce706bc0ddc5b7ac1624bac74fee0
Mike North <northm@yahoo-inc.com>
2015-04-13 01:38:06 -0700

    Pagination component

---
+++ addon/components/materialize-pagination.js
@@ -0,0 +1,77 @@
+import Ember from 'ember';
+import layout from '../templates/components/materialize-pagination';
+
+export default Ember.Component.extend({
+  min: 1,
+  max: 1,
+  current: 1,
+  range: 5,
+  layout: layout,
+  tagName: 'ul',
+  classNames: ['pagination'],
+
+  windowRange: Ember.computed('min', 'max', 'range', 'current', function () {
+    var max = this.get('max');
+    var min = this.get('min');
+    var range = this.get('range');
+    var current = this.get('current');
+
+    var middle = Math.floor((max - min)/2);
+    var low = Math.max(min, current - Math.floor(range/2));
+    var high = Math.min(max, current + Math.floor(range/2));
+
+    if (high-low < range-1) {
+      if (current <= middle) {
+        high = Math.min(max, low + range-1);
+      }
+      else {
+        low = Math.max(min, high - (range-1));
+      }
+    }
+    return {
+      low, high
+    };
+  }),
+
+  _pages: Ember.computed('windowRange.low', 'windowRange.high', 'current', function () {
+    var a = Ember.A();
+    var winRange = this.get('windowRange');
+    var current = this.get('current');
+    for (var i = winRange.low; i <= winRange.high; i +=1) {
+      a.addObject({val: i, cssClass: (current === i ? 'active' : 'waves-effect')});
+    }
+    return a;
+  }).readOnly(),
+
+  _canGoBack: Ember.computed('min', 'current', function () {
+    return this.get('current') > this.get('min');
+  }),
+
+  _canGoFwd: Ember.computed('max', 'current', function () {
+    return this.get('current') < this.get('max');
+  }),
+
+  incrementClass: Ember.computed('_canGoFwd', function() {
+    return this.get('_canGoFwd') ? '' : 'disabled';
+  }),
+
+  decrementClass: Ember.computed('_canGoBack', function() {
+    return this.get('_canGoBack') ? '' : 'disabled';
+  }),
+
+  actions: {
+    oneBack() {
+      if (this.get('_canGoBack')) {
+        this.decrementProperty('current');
+      }
+    },
+    oneFwd() {
+      if (this.get('_canGoFwd')) {
+        this.incrementProperty('current');
+      }
+    },
+    gotoPage(pagenum) {
+      this.set('current', pagenum);
+    }
+  }
+});
---
+++ tests/unit/components/materialize-pagination-test.js
@@ -0,0 +1,88 @@
+import Ember from 'ember';
+
+import {
+  moduleForComponent,
+  test
+} from 'ember-qunit';
+
+moduleForComponent('materialize-pagination', {
+  // Specify the other units that are required for this test
+  // needs: ['component:foo', 'helper:bar']
+});
+
+test('it renders', function(assert) {
+  assert.expect(2);
+
+  // Creates the component instance
+  var component = this.subject();
+  assert.equal(component._state, 'preRender');
+
+  // Renders the component to the page
+  this.render();
+  assert.equal(component._state, 'inDOM');
+});
+
+test('window range calculation', function(assert) {
+  assert.expect(3);
+
+  // Creates the component instance
+  var component = this.subject({
+    min: 1,
+    max: 12,
+    current: 2,
+    range: 5
+  });
+  assert.deepEqual(component.get('windowRange'), {low: 1, high: 5}, 'Off center window range calculates correctly');
+  Ember.run(function () {
+
+    component.setProperties({
+      min: 1,
+      max: 12,
+      current: 7,
+      range: 5
+    });
+    assert.deepEqual(component.get('windowRange'), {low: 5, high: 9}, 'Changes to current position updates window range');
+
+    component.setProperties({
+      min: 1,
+      max: 12,
+      current: 7,
+      range: 7
+    });
+    assert.deepEqual(component.get('windowRange'), {low: 4, high: 10}, 'Changes to range width updates window range');
+
+  });
+
+});
+
+test('increment button', function(assert) {
+  assert.expect(5);
+
+  // Creates the component instance
+  var component = this.subject({
+    min: 1,
+    max: 12,
+    current: 2,
+    range: 5
+  });
+  this.render();
+  Ember.run( function () {
+    component.$('.increment').click();
+    assert.equal(component.get('current'), 3, 'Increment button incremenets position');
+    component.$('.decrement').click();
+    component.$('.decrement').click();
+    assert.equal(component.get('current'), 1, 'Decrement button incremenets position');
+    assert.equal(component.$('li.disabled').length, 1, 'Decrement button disables at lower edge of range');
+    component.set('current', 7);
+    Ember.run.schedule('afterRender', function () {
+      assert.equal(component.$('li.disabled').length, 0, 'No buttons disabled in middle of range');
+      component.set('current', 12);
+      Ember.run.schedule('afterRender', function () {
+        assert.equal(component.$('li.disabled').length, 1, 'Increment button disables at upper edge of range');
+      });
+    });
+
+  });
+
+});
+
