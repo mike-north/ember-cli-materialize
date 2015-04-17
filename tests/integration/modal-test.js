24db0b09e93628557fa57ce56c9b82f36385d6d0
Mike North <northm@yahoo-inc.com>
2015-04-17 00:45:53 -0700

    More idiomatic tests

--- tests/integration/modal-test.js
+++ tests/integration/modal-test.js
@@ -15,7 +15,6 @@ module('Acceptance - Modal', {
 });
 
 test('Modal container is installed', function(assert) {
-  expect(1);
   visit('/modal');
 
   andThen(function () {
@@ -25,10 +24,13 @@ test('Modal container is installed', function(assert) {
 
 
 test('Modal opens when button is clicked', function(assert) {
-  expect(2);
-  visit('/modal').then(function () {
+  visit('/modal');
+
+  andThen(function () {
     assert.equal(Ember.$('#materialize-modal-root-element .modal-content').length, 0, 'Modal is on not the screen');
-    click('.open-modal-button').then(function () {
+    click('.open-modal-button');
+
+    andThen(function () {
       assert.equal(Ember.$('#materialize-modal-root-element .modal-content').length, 1, 'Modal is on the screen');
     });
   });
@@ -36,12 +38,17 @@ test('Modal opens when button is clicked', function(assert) {
 
 
 test('Modal is dismissed upon hitting "cancel"', function(assert) {
-  expect(3);
-  visit('/modal').then(function () {
+  visit('/modal');
+
+  andThen(function () {
     assert.equal(Ember.$('#materialize-modal-root-element .modal-content').length, 0, 'Modal is on not the screen');
-    click('.open-modal-button').then(function () {
+    click('.open-modal-button');
+
+    andThen(function () {
       assert.equal(Ember.$('#materialize-modal-root-element .modal-content').length, 1, 'Modal is on the screen');
-      click(Ember.$('#materialize-modal-root-element .modal-footer .cancel-button')).then(function () {
+      click(Ember.$('#materialize-modal-root-element .modal-footer .cancel-button'));
+
+      andThen(function () {
         assert.equal(Ember.$('#materialize-modal-root-element .modal-content').length, 0, 'Modal is on not the screen');
       });
     });
@@ -50,12 +57,17 @@ test('Modal is dismissed upon hitting "cancel"', function(assert) {
 
 
 test('Modal is dismissed by clicking on background', function(assert) {
-  expect(3);
-  visit('/modal').then(function () {
+  visit('/modal');
+
+  andThen(function () {
     assert.equal(Ember.$('#materialize-modal-root-element .modal-content').length, 0, 'Modal is on not the screen');
-    click('.open-modal-button').then(function () {
+    click('.open-modal-button');
+
+    andThen(function () {
       assert.equal(Ember.$('#materialize-modal-root-element .modal-content').length, 1, 'Modal is on the screen');
-      click(Ember.$('#lean-overlay')).then(function () {
+      click(Ember.$('#lean-overlay'));
+
+      andThen(function () {
         assert.equal(Ember.$('#materialize-modal-root-element .modal-content').length, 0, 'Modal is on not the screen');
       });
     });
