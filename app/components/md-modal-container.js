7ad011bbe32577217e8620ef1422a2d778f1c078
Mike North <northm@yahoo-inc.com>
2015-04-23 23:43:03 -0700

    Modal container component, modal documentation improvements

---
+++ app/components/md-modal-container.js
@@ -0,0 +1,3 @@
+import mdModalContainer from 'ember-cli-materialize/components/md-modal-container';
+
+export default mdModalContainer;
---
+++ tests/unit/components/md-modal-container-test.js
@@ -0,0 +1,32 @@
+import {
+  moduleForComponent,
+  test
+} from 'ember-qunit';
+
+moduleForComponent('md-modal-container', {
+  // Specify the other units that are required for this test
+  // needs: ['component:foo', 'helper:bar']
+});
+
+test('it renders', function(assert) {
+  assert.expect(2);
+
+  // Creates the component instance
+  var component = this.subject();
+  assert.equal(component._state, 'preRender');
+
+  // Renders the component to the page
+  this.render();
+  assert.equal(component._state, 'inDOM');
+});
+
+
+test('it has the expected ID for md-modal to render into it', function(assert) {
+  assert.expect(1);
+
+  // Creates the component instance
+  var component = this.subject();
+  // Renders the component to the page
+  this.render();
+  assert.equal(component.$('#materialize-modal-root-element').length, 1, '#materialize-modal-root-element should be in the DOM');
+});
