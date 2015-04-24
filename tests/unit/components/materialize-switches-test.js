0b19acadf95e796875c80cabdb3ee98cfa18c191
Mike North <northm@yahoo-inc.com>
2015-04-23 17:14:25 -0700

    Select bugfix, and test coverage for simple array use cases. Fixes #74

--- tests/unit/components/materialize-switches-test.js
+++ tests/unit/components/materialize-switches-test.js
@@ -29,6 +29,16 @@ test('it renders', function(assert) {
   assert.equal(component._state, 'inDOM');
 });
 
+test('simple array test', function (assert) {
+  var component = this.subject({
+    content: Ember.A(['Dexter Morgan', 'Deborah Morgan', 'Harry Morgan']),
+    selection: Ember.A(['Deborah Morgan'])
+  });
+  this.render();
+  assert.deepEqual(this.$('.switch-label').toArray().map(x => Ember.$(x).text()), ['Dexter Morgan', 'Deborah Morgan', 'Harry Morgan'], 'Choices are valid');
+  assert.equal(this.$('input[type="checkbox"]')[1].checked, true, 'Second checkbox is checked');
+});
+
 disabledGroupTest();
 groupItemsRenderTest();
 initialSelectionTest(Ember.A(['bbb', 'ccc']));
