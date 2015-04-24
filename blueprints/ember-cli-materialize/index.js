1e23fcdef9098976c4b7c7d7aa818970ada26974
Mike North <northm@yahoo-inc.com>
2015-04-23 19:59:38 -0700

    Add nested addons to blueprint

--- blueprints/ember-cli-materialize/index.js
+++ blueprints/ember-cli-materialize/index.js
@@ -6,6 +6,14 @@ module.exports = {
   },
 
   afterInstall: function() {
-    return this.addPackageToProject('ember-cli-sass', '^3.3.0');
+    return this.addPackageToProject('ember-radio-button',   '1.0.4').then(function () {
+      return this.addPackageToProject('ember-new-computed',   '~1.0.0').then(function () {
+        return this.addPackageToProject('ember-key-responder',  '0.2.1').then(function () {
+          return this.addPackageToProject('ember-modal-dialog',   '0.3.0').then(function () {
+            return this.addPackageToProject('ember-cli-sass',       '^3.3.0');
+          }.bind(this));
+        }.bind(this));
+      }.bind(this));
+    }.bind(this));
   }
 };
