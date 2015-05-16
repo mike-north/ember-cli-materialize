ea61c990ba00d85ff8968c7a489842c57a099d34
Mike North <northm@yahoo-inc.com>
2015-05-16 14:44:05 -0700

    move dependencies to package.json

--- blueprints/ember-cli-materialize/index.js
+++ blueprints/ember-cli-materialize/index.js
@@ -3,17 +3,18 @@ module.exports = {
 
   beforeInstall: function(options) {
     return this.addBowerPackageToProject('materialize', '~0.96.0');
-  },
-
-  afterInstall: function() {
-    return this.addPackageToProject('ember-radio-button',   '1.0.4').then(function () {
-      return this.addPackageToProject('ember-new-computed',   '~1.0.0').then(function () {
-        return this.addPackageToProject('ember-key-responder',  '0.2.1').then(function () {
-          return this.addPackageToProject('ember-modal-dialog',   '0.3.0').then(function () {
-            return this.addPackageToProject('ember-cli-sass',       '^3.3.0');
-          }.bind(this));
-        }.bind(this));
-      }.bind(this));
-    }.bind(this));
   }
+  // },
+
+  // afterInstall: function() {
+  //   return this.addPackageToProject('ember-radio-button',   '1.0.4').then(function () {
+  //     return this.addPackageToProject('ember-new-computed',   '~1.0.0').then(function () {
+  //       return this.addPackageToProject('ember-key-responder',  '0.2.1').then(function () {
+  //         return this.addPackageToProject('ember-modal-dialog',   '0.3.0').then(function () {
+  //           return this.addPackageToProject('ember-cli-sass',       '^3.3.0');
+  //         }.bind(this));
+  //       }.bind(this));
+  //     }.bind(this));
+  //   }.bind(this));
+  // }
 };
