08d6f740866981fce5afa8c849690f21f434021e
Mike North <michael.l.north@gmail.com>
2015-05-23 09:34:12 -0700

    Update ember-modal-dialog to 0.7.0
    Fixes #115

--- blueprints/ember-cli-materialize/index.js
+++ blueprints/ember-cli-materialize/index.js
@@ -9,7 +9,7 @@ module.exports = {
     return this.addPackageToProject('ember-radio-button',   '1.0.4').then(function () {
       return this.addPackageToProject('ember-new-computed',   '~1.0.0').then(function () {
         return this.addPackageToProject('ember-key-responder',  '0.2.1').then(function () {
-          return this.addPackageToProject('ember-modal-dialog',   '0.3.0').then(function () {
+          return this.addPackageToProject('ember-modal-dialog',   '0.7.0').then(function () {
             return this.addPackageToProject('ember-cli-sass',       '^3.3.0');
           }.bind(this));
         }.bind(this));
