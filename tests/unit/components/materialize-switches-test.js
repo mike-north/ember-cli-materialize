c12535c0ecf4af483981f5c11076744b2f1fd1ef
Lisandro Mc Gough <lisandro@blink.local>
2015-06-10 13:15:33 -0300

    Add regression tests for #133
    https://github.com/sgasser/ember-cli-materialize/issues/133
    Cannot deselect item with md-checks and others if multiple=false

--- tests/unit/components/materialize-switches-test.js
+++ tests/unit/components/materialize-switches-test.js
@@ -12,6 +12,11 @@ import {
 } from '../../helpers/selectable-item';
 
 
+import {
+  deselectForSingleSelectionTest,
+} from '../../helpers/selectable-item-group';
+
+
 moduleForComponent('md-switches', {
   unit: true
   // Specify the other units that are required for this test
@@ -43,3 +48,5 @@ test('simple array test', function (assert) {
 disabledGroupTest();
 groupItemsRenderTest();
 initialSelectionTest(Ember.A(['bbb', 'ccc']));
+
+deselectForSingleSelectionTest();
