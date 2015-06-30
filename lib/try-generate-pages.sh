d2759bd569c1255935cff0124eefb62bbc89ac8b
Mike North <northm@yahoo-inc.com>
2015-06-30 11:57:18 -0700

    fix gh-pages deploy script

--- lib/try-generate-pages.sh
+++ lib/try-generate-pages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Attempting to generate pages"
-if [[ "${TRAVIS_PULL_REQUEST}" = "false" && "${TRAVIS_BRANCH}" == 'main' ]]; then
+if [[ "${TRAVIS_PULL_REQUEST}" = "false" && "${TRAVIS_BRANCH}" == 'master' ]]; then
   ember github-pages:commit --message "New release" --branch "${GH_DEST_BRANCH}"
   git push deploy gh-pages 2>&1 >/dev/null
 else
