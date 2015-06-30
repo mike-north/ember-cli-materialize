5c955e89221c31963d884e84e29b94671f7f2468
Mike North <northm@yahoo-inc.com>
2015-06-30 10:38:56 -0700

    github pages auto-delpoy

---
+++ lib/try-generate-pages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Attempting to generate pages"
+if [[ "${TRAVIS_PULL_REQUEST}" = "false" && "${TRAVIS_BRANCH}" == 'main' ]]; then
+  ember github-pages:commit --message "New release" --branch "${GH_DEST_BRANCH}"
+  git push deploy gh-pages 2>&1 >/dev/null
+else
+  echo "Not a main build -- no deploy"
+fi
