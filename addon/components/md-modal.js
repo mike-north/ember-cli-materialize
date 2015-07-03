391413cf46d90838639ce9f06c253554b5edcba1
Mike North <michael.l.north@gmail.com>
2015-07-03 12:20:26 -0700

    Modal fixed footer

--- addon/components/md-modal.js
+++ addon/components/md-modal.js
@@ -9,10 +9,24 @@ export default YappModal.extend({
   acceptsKeyResponder: true,
   overlayId: 'lean-modal',
   attributeBindings: ['style:inlineStyle'],
+  concatenatedProperties: ['modalClassNames'],
 
   inlineStyle: computed({
     get() {
       return 'z-index: 1000;';
+    }
+  }),
+
+  fixedFooter: false,
+
+  modalClassNames: ['modal', 'show'],
+  _modalClassString: computed('modalClassNames.@each', 'fixedFooter', {
+    get() {
+      let names = this.get('modalClassNames');
+      if (this.get('fixedFooter')) {
+        names.push('modal-fixed-footer');
+      }
+      return names.join(' ');
     }
   }),
 
