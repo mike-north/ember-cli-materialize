c75d40b0defeb7a33e548724889044461d8f3edf
Mike North <michael.l.north@gmail.com>
2015-08-16 12:20:33 -0700

    Tests check for deprecations

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -37,6 +37,7 @@ module.exports = function(environment) {
     // Testem prefers this...
     ENV.baseURL = '/';
     ENV.locationType = 'none';
+    ENV.EmberENV.RAISE_ON_DEPRECATION = !process.env['ALLOW_DEPRECATIONS'];
 
     // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
