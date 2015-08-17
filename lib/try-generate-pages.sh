115f6b61090e39d82ddfab3225a5e0d4c30d5497
Mike North <michael.l.north@gmail.com>
2015-08-16 21:32:49 -0700

    gh-pages demo in ember-1.12

--- lib/try-generate-pages.sh
+++ lib/try-generate-pages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Attempting to generate pages"
-if [[ "${TRAVIS_PULL_REQUEST}" = "false" && "${TRAVIS_BRANCH}" == 'master' ]]; then
+if [[ "${TRAVIS_PULL_REQUEST}" = "false" && "${TRAVIS_BRANCH}" == 'master' && "${EMBER_TRY_SCENARIO}" == 'ember-1.12' ]]; then
   ember github-pages:commit --message "New release" --branch "${GH_DEST_BRANCH}"
   git push deploy gh-pages 2>&1 >/dev/null
 else
