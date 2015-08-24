92d262e715fa160b4cdec3487f591781a8764ad0
Ryan Tablada <ryan.tablada@gmail.com>
2015-08-24 09:48:33 -0500

    Added legacy views to added NPM packages to get rid of view warnings

--- blueprints/ember-cli-materialize/index.js
+++ blueprints/ember-cli-materialize/index.js
@@ -14,7 +14,8 @@ module.exports = {
       this.addPackageToProject('ember-new-computed', '~1.0.0'),
       this.addPackageToProject('ember-key-responder', '0.2.1'),
       this.addPackageToProject('ember-modal-dialog', '~0.7.5'),
-      this.addPackageToProject('ember-cli-sass', '^3.3.0')
+      this.addPackageToProject('ember-cli-sass', '^3.3.0'),
+      this.addPackageToProject('ember-legacy-views', '^0.2.0')
     ]);
   }
 }
