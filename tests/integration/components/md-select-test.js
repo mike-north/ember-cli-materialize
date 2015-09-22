ba7b409bb6ebe03a91cd4874ca9cafed5b7cdded
Mike North <michael.l.north@gmail.com>
2015-09-22 00:10:49 -0700

    Fix broken md-select tests

---
+++ tests/integration/components/md-select-test.js
@@ -0,0 +1,52 @@
+import Ember from 'ember';
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('md-select', 'Integration | Component | md select', {
+  integration: true
+});
+
+test('basic usage', function(assert) {
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.set('frameworks', new Ember.A([
+      { id: 1, value: 'Materialize CSS' },
+      { id: 2, value: 'Ember-CLI Materialize' }
+  ]));
+  // Template block usage:
+  this.render(hbs`
+    {{md-select content=frameworks
+      value=framework
+      label="Framework"
+      prompt="Please choose..."
+      optionLabelPath="content.value"
+      optionValuePath="content" class="col s12"}}
+  `);
+
+  assert.equal(this.$().text().trim(), `Framework
+▼Please choose...Materialize CSSEmber-CLI MaterializePlease choose...Materialize CSSEmber-CLI Materialize`);
+
+  assert.ok(this.$('.md-select').hasClass('input-field'));
+
+  assert.equal(this.$('.md-select >label').text(), 'Framework');
+});
+
+test('simple array usage', function(assert) {
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.set('content', new Ember.A(['Walter White', 'Jesee Pinkman', 'Gus Freng']));
+  // Template block usage:
+  this.render(hbs`
+    {{md-select
+      content=content
+      value="Jesee Pinkman"}}
+  `);
+
+  assert.equal(this.$().text().trim(), '▼Walter WhiteJesee PinkmanGus FrengWalter WhiteJesee PinkmanGus Freng');
+  assert.equal(this.$('.md-select input').val(), 'Jesee Pinkman', 'Jesee Pinkman is initially selected');
+
+});
