cef9d24182cb497f0cef213daf7614f5075f93d4
Ramy Ben Aroya <ramybenaroya@gmail.com>
2016-01-27 10:21:04 +0200

    minor

--- addon/components/md-input-date.js
+++ addon/components/md-input-date.js
@@ -29,8 +29,8 @@ export default MaterializeInput.extend({
   },
 
   _setupPicker() {
-    const datePickerOptions = this.getProperties('selectMonths', 'numberOfYears');
-    datePickerOptions.container = this.get('appendTo');
+    const datePickerOptions = this.getProperties('selectMonths', 'numberOfYears', 'appendTo');
+    datePickerOptions.container = datePickerOptions.appendTo;
     datePickerOptions.selectYears = datePickerOptions.numberOfYears;
 
     var _onDateSet = evt => {
