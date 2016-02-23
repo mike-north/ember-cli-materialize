c24158ac5571139226e844ab34a8dcf265c8fc82
Ramy Ben Aroya <ramybenaroya@gmail.com>
2016-02-23 18:15:01 +0200

    md-modal overlayClass prop

--- addon/components/md-modal.js
+++ addon/components/md-modal.js
@@ -12,6 +12,8 @@ export default Component.extend(UsesSettings, {
   attributeBindings: ['style:inlineStyle'],
   concatenatedProperties: ['modalClassNames'],
 
+  overlayClass: 'lean-modal',
+
   inlineStyle: computed(function() {
     return new Ember.Handlebars.SafeString('z-index: 1000;');
   }),
