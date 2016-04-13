c598d67250ab3fd3cdadad94adb42e273d1816e1
Ramy Ben Aroya <ramybenaroya@gmail.com>
2016-04-13 16:32:17 +0300

    restore lost md-modal code

--- addon/components/md-modal.js
+++ addon/components/md-modal.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import $ from 'jquery';
 import UsesSettings from '../mixins/uses-settings';
 import layout from '../templates/components/md-modal';
 import { EKMixin } from 'ember-keyboard';
@@ -11,6 +12,8 @@ export default Component.extend(EKMixin, UsesSettings, {
   keyboardFirstResponder: true,
   attributeBindings: ['style:inlineStyle'],
   concatenatedProperties: ['modalClassNames'],
+
+  overlayClassName: 'lean-modal',
 
   inlineStyle: computed(function() {
     return new Ember.Handlebars.SafeString('z-index: 1000;');
@@ -27,14 +30,47 @@ export default Component.extend(EKMixin, UsesSettings, {
     return names.join(' ');
   }),
 
+  didInsertElement() {
+    this._super(...arguments);
+    var onInsert = this.getAttr('onInsert');
+    var bodyClassName = this.getAttr('bodyClassName');
+    if (bodyClassName) {
+      $('body').addClass(bodyClassName);
+    }
+    if (onInsert) {
+      onInsert();
+    }
+  },
+
+  willDestroyElement() {
+    this._super(...arguments);
+    var onDestroy = this.getAttr('onDestroy');
+    var bodyClassName = this.getAttr('bodyClassName');
+    if (bodyClassName) {
+      $('body').removeClass(bodyClassName);
+    }
+    if (onDestroy) {
+      onDestroy();
+    }
+  },
+
   cancel() {
-    this.sendAction('close');
+    this.closeModal();
   },
 
   actions: {
     closeModal() {
-      this.sendAction('close');
+      this.closeModal();
+    }
+  },
+
+  closeModal(){
+    if (this.getAttr('onClose')){
+      this.getAttr('onClose')();
+    } else {
+      this.sendAction('close');  
     }
   }
 
 });
+
