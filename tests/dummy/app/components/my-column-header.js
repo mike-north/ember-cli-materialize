3f6928240da099a451890d3907a94d76e54b0186
Ramy Ben Aroya <ramybenaroya@gmail.com>
2016-04-13 14:22:22 +0300

    replace default-column-header view with component

---
+++ tests/dummy/app/components/my-column-header.js
@@ -0,0 +1,9 @@
+import layout from '../templates/snippets/my-column-header';
+
+// BEGIN-SNIPPET my-column-header
+import DefaultColumnHeaderComponent from 'ember-cli-materialize/components/md-default-column-header';
+
+export default DefaultColumnHeaderComponent.extend({
+  layout
+});
+// END-SNIPPET
