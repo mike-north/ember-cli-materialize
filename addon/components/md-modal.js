ee1877fa51eee9cd336be11ed5dc3aff712b3507
Ramy Ben Aroya <ramybenaroya@gmail.com>
2016-04-13 16:13:08 +0300

    use ember-keyboard

--- addon/components/md-modal.js
+++ addon/components/md-modal.js
@@ -1,18 +1,16 @@
 import Ember from 'ember';
-import $ from 'jquery';
 import UsesSettings from '../mixins/uses-settings';
 import layout from '../templates/components/md-modal';
+import { EKMixin } from 'ember-keyboard';
 
 const { Component, computed, computed: { oneWay } } = Ember;
 
-export default Component.extend(UsesSettings, {
+export default Component.extend(EKMixin, UsesSettings, {
   layout,
 
-  acceptsKeyResponder: true,
+  keyboardFirstResponder: true,
   attributeBindings: ['style:inlineStyle'],
   concatenatedProperties: ['modalClassNames'],
-
-  overlayClassName: 'lean-modal',
 
   inlineStyle: computed(function() {
     return new Ember.Handlebars.SafeString('z-index: 1000;');
@@ -29,47 +27,13 @@ export default Component.extend(UsesSettings, {
     return names.join(' ');
   }),
 
-  didInsertElement() {
-    this._super(...arguments);
-    this.becomeKeyResponder();
-    var onInsert = this.getAttr('onInsert');
-    var bodyClassName = this.getAttr('bodyClassName');
-    if (bodyClassName) {
-      $('body').addClass(bodyClassName);
-    }
-    if (onInsert) {
-      onInsert();
-    }
-  },
-
-  willDestroyElement() {
-    this._super(...arguments);
-    this.resignKeyResponder();
-    var onDestroy = this.getAttr('onDestroy');
-    var bodyClassName = this.getAttr('bodyClassName');
-    if (bodyClassName) {
-      $('body').removeClass(bodyClassName);
-    }
-    if (onDestroy) {
-      onDestroy();
-    }
-  },
-
   cancel() {
-    this.closeModal();
+    this.sendAction('close');
   },
 
   actions: {
     closeModal() {
-      this.closeModal();
-    }
-  },
-
-  closeModal(){
-    if (this.getAttr('onClose')){
-      this.getAttr('onClose')();
-    } else {
-      this.sendAction('close');  
+      this.sendAction('close');
     }
   }
 
--- blueprints/ember-cli-materialize/index.js
+++ blueprints/ember-cli-materialize/index.js
@@ -12,10 +12,10 @@ module.exports = {
       this.addPackageToProject('ember-composability', '~0.1.1'),
       this.addPackageToProject('ember-radio-button', '1.0.7'),
       this.addPackageToProject('ember-new-computed', '~1.0.0'),
-      this.addPackageToProject('ember-key-responder', '~0.4.0'),
       this.addPackageToProject('ember-modal-dialog', '~0.7.5'),
       this.addPackageToProject('ember-cli-sass', '^3.3.0'),
-      this.addPackageToProject('ember-truth-helpers', '~1.2.0')
+      this.addPackageToProject('ember-truth-helpers', '~1.2.0'),
+      this.addPackageToProject('ember-keyboard', '^1.0.3')
     ]);
   }
 }
