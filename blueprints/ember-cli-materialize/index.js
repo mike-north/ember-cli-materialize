ba0b301412b7ca71d322f9dfe9d7eb0b471c6498
Mike North <michael.l.north@gmail.com>
2016-04-22 21:30:09 +0200

    Use external shims for materialize and md-icons

--- blueprints/ember-cli-materialize/index.js
+++ blueprints/ember-cli-materialize/index.js
@@ -16,6 +16,8 @@ module.exports = {
       this.addPackageToProject('ember-truth-helpers', '1.2.0'),
       this.addPackageToProject('ember-modal-dialog', '~0.8.0'),
       this.addPackageToProject('ember-cli-sass', 'mike-north/ember-cli-sass#5e0c40ace35ef185e78f9ea921a5e3847958bc0c'),
+      this.addPackageToProject('ember-material-design-icons-shim', '0.1.1'),
+      this.addPackageToProject('ember-materialize-shim', '0.1.1'),
       this.addPackageToProject('ember-legacy-views', '^0.2.0')
     ]);
   }
