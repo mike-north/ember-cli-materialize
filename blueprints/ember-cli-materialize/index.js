22fee3a86a0d99012b8bb1c78d2770b6b2bb9fe9
Dustin Farris <dustin.farris@gmail.com>
2016-07-27 09:02:18 -0400

    Upgrade blueprint materialize shim to ~0.2.2

--- blueprints/ember-cli-materialize/index.js
+++ blueprints/ember-cli-materialize/index.js
@@ -7,7 +7,7 @@ module.exports = {
   normalizeEntityName: function() {},
   afterInstall: function(options) {
     return this.addAddonsToProject({packages: [
-      {name: 'ember-materialize-shim', target: '~0.1.5'},
+      {name: 'ember-materialize-shim', target: '~0.2.2'},
       {name: 'ember-cli-flash', target: '^1.3.14'},
       {name: 'ember-gestures',  target:'~0.4.1'}
     ]});
