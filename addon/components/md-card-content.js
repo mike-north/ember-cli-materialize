008c81f58df41828cf3eb59acfff198562b3b088
Jose Maria Ruiz Aguilera <josemariaruiz@gmail.com>
2016-09-13 10:25:28 +0100

    Update code to comply with Ember 2.8.0.
    Check discussion in https://github.com/emberjs/ember.js/issues/14266

--- addon/components/md-card-content.js
+++ addon/components/md-card-content.js
@@ -1,6 +1,5 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
-import { alias } from '@ember/object/computed';
 import layout from '../templates/components/md-card-content';
 
 export default Component.extend({
@@ -9,9 +8,18 @@ export default Component.extend({
   classNames: ['card-content'],
 
   classNameBindings: ['class'],
-  title: alias('parentView.title'),
-  titleClass: alias('parentView.titleClass'),
-  activator: alias('parentView.activator'),
+
+  title: computed(function() {
+    return this.get('parentView.title');
+  }).volatile(),
+
+  titleClass: computed(function() {
+    return this.get('parentView.titleClass');
+  }).volatile(),
+
+  activator: computed(function() {
+    return this.get('parentView.activator');
+  }),
 
   cardTitleClass: computed('titleClass', function() {
     return this.get('titleClass') || 'black-text';
