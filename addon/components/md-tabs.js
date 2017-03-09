85502b37a1a16ad570911edb68e147058cd5db60
Kyle Rose <esornona@gmail.com>
2017-03-08 16:11:10 -0800

    on window resize, change the tab indicator size

--- addon/components/md-tabs.js
+++ addon/components/md-tabs.js
@@ -29,7 +29,9 @@ export default Component.extend(ParentComponentSupport, {
 
   didInsertElement() {
     this._super(...arguments);
-    this._updateIndicatorPosition(false);
+    this.$(window).resize(() => {
+      this._updateIndicatorPosition(false);
+    });
   },
 
   _indicatorUpdater: observer('selected', 'content.[]', 'composableChildren.[]', function() {
