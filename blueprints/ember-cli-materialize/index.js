072d72a95f2ba16acd9c9ab2dfa6780b6837beef
Kieran Cahir <kcahir@ymail.com>
2019-02-28 02:09:51 +0000

    feat: remove ember-new-computed dependency

--- blueprints/ember-cli-materialize/index.js
+++ blueprints/ember-cli-materialize/index.js
@@ -8,7 +8,6 @@ module.exports = {
       packages: [
         { name: 'ember-composability', target: '~0.3.7' },
         { name: 'ember-radio-button', target: '1.0.7' },
-        { name: 'ember-new-computed', target: '~1.0.2' },
         { name: 'ember-keyboard', target: '3.0.0' },
         { name: 'ember-truth-helpers', target: '1.2.0' },
         { name: 'ember-modal-dialog', target: '~0.8.0' },
--- tests/dummy/app/components/options-panel.js
+++ tests/dummy/app/components/options-panel.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
-import computed from 'ember-new-computed';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   componentName: '',
